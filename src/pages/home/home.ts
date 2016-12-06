2d5129ccd5a06b5bfcb2e31738134b243d187895
Professor Raghunath <raghunat@fredonia.edu>
2016-12-06 09:38:47 -0500

    Adding base animations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  Component, style, animate, transition, state,
+  trigger
+} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
+
+declare var io;
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(-5%)', opacity: 0}),
+        animate(500)
+      ])
+    ])
+  ]
 })
 export class HomePage {
+
+  messages = [];
+  message: string = '';
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  sendMessage(message) {
+    this.messages.push(message);
+    this.message = '';
+  }
 }
