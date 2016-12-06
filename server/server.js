619e91ad530d46c71f03e07add0176c30f3bf731
Professor Raghunath <raghunat@fredonia.edu>
2016-12-06 09:20:01 -0500

    Adding Base server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by stephenraghunath on 12/6/16.
+ */
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('www'));
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(process.env.PORT || 3000, function() {
+  console.log('listening on *:3000');
+});
